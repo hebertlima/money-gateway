72eaad43fd1c6cb860879c5b468d2e8000f160cc
Hebert Lima <hebert.dev@hotmail.com>
2021-07-21 11:54:31 -0300

    added icon

--- migrations/2021_06_17_184734_money_gateway.php
+++ migrations/2021_06_17_184734_money_gateway.php
@@ -16,6 +16,7 @@ class MoneyGateway extends Migration
         if (Schema::hasTable('gateways')) {
             \DB::table('gateways')->insert([
                 'code' => 'money-gateway',
+                'icon' => '/vendor/hdelima/MoneyGateway/public/icon.png',
                 'label' => 'Money',
                 'enabled' => 0,
                 'settings' => null,
